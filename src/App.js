6654e70b936572df7bda241c300be4b67ec87ad1
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-07-09 10:30:50 -0300

    tela de compra e login

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import "./assets/styles/reset.css";
+import GlobalStyle from "./assets/styles/globalStyle";
+import InitialScreen from "./components/InitialScreen";
+import Login from "./components/Login";
+import BuySession from "./components/BuySession";
+
+export default function App() { 
+    return( 
+        <>
+        <GlobalStyle />
+        <BrowserRouter>
+            <Routes> 
+                <Route path="/" element={<InitialScreen />} />  
+                <Route path="/login" element={<Login />} />  
+                <Route path="/buy" element={<BuySession />} />
+            </Routes> 
+        </BrowserRouter> 
+        </>
+    )
+}
